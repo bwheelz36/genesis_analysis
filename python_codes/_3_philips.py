e417c94ba76ad3f11aa97ce525d9c01f3b96aa6d
unknown <bwheelz360@gmail.com>
2022-09-15 01:38:48 +1000

    added in some reporting to phillips examples and made the notebook write up

--- python_codes/_3_philips.py
+++ python_codes/_3_philips.py
@@ -1,11 +1,10 @@
 from pathlib import Path
 from mri_distortion_toolkit.MarkerAnalysis import MarkerVolume,MatchedMarkerVolumes
-from time import perf_counter
-from philips_phantom import philips_phantom
 from mri_distortion_toolkit.utilities import plot_distortion_xyz_hist
+from philips_phantom import philips_phantom
 
 this_file_loc = Path(__file__).parent.resolve()
-data_loc = this_file_loc / '_segmentation_data'
+data_loc = this_file_loc.parent / '_segmentation_data'
 
 # Distorted centroids corrected
 philips_MR_corrected = MarkerVolume(data_loc / 'PhilipPhantom_MRI' / '2022-08__Studies\GOAM^ImageX_ZZZIMAGEX_MR_2022-08-31_172341_._T2.3D.Tra.2min_n401__00002' / 'slicer_centroids.mrk.json')
@@ -21,9 +20,9 @@ philips_gt = MarkerVolume(philips_centroids)
 # philips_gt.plot_3D_markers(title='Ground truth')
 
 # # Match markers corrected
-# corrected = MatchedMarkerVolumes(philips_gt, philips_MR_corrected)
-# corrected.plot_3D_markers()
-# plot_distortion_xyz_hist(corrected)
+corrected = MatchedMarkerVolumes(philips_gt, philips_MR_corrected)
+corrected.plot_3D_markers()
+plot_distortion_xyz_hist(corrected)
 
 # Distorted centroids uncorrected
 philips_MR_uncorrected = MarkerVolume(data_loc / 'PhilipPhantom_MRI' / '2022-08__Studies\GOAM^ImageX_ZZZIMAGEX_MR_2022-08-31_172341_._T2.3D.Tra.2min_n401__00000' / 'slicer_centroids.mrk.json')
@@ -35,3 +34,32 @@ uncorrected = MatchedMarkerVolumes(philips_gt, philips_MR_uncorrected)
 uncorrected.plot_3D_markers()
 plot_distortion_xyz_hist(uncorrected)
 
+print(f'Median distortion: {uncorrected.MatchedCentroids.match_distance.median(): 1.1f} mm, '
+      f'Max distortion: {uncorrected.MatchedCentroids.match_distance.max(): 1.1f} mm')
+
+from mri_distortion_toolkit.Reports import MRI_QA_Reporter
+from mri_distortion_toolkit.Reports import Elekta_Distortion_tests
+
+report = MRI_QA_Reporter(MatchedMarkerVolume=corrected.MatchedCentroids,
+                         r_outer=190,
+                         dicom_data=philips_MR_uncorrected.dicom_data,
+                         tests_to_run=Elekta_Distortion_tests)
+# report.write_html_report()
+
+from mri_distortion_toolkit.FieldCalculation import ConvertMatchedMarkersToBz
+from mri_distortion_toolkit import calculate_harmonics
+import numpy as np
+
+Bz = ConvertMatchedMarkersToBz(uncorrected.MatchedCentroids, philips_MR_uncorrected.dicom_data)
+gradient_strength = np.array(philips_MR_uncorrected.dicom_data['gradient_strength'])
+normalisation_factor = [1 / gradient_strength[0], 1 / gradient_strength[1], 1 / gradient_strength[2], 1]
+# this normalised gradient harmonics to 1mT/m
+G_x_Harmonics, G_y_Harmonics, G_z_Harmonics, B0_Harmonics = calculate_harmonics(Bz.MagneticFields,
+                                                                                n_order=5,
+                                                                                norm=normalisation_factor)
+
+report_harmonics = MRI_QA_Reporter(gradient_harmonics=[G_x_Harmonics.harmonics, G_y_Harmonics.harmonics, G_z_Harmonics.harmonics],
+                         r_outer=190,
+                         dicom_data=philips_MR_uncorrected.dicom_data,
+                         tests_to_run=Elekta_Distortion_tests)
+report_harmonics.write_html_report()
--- python_codes/_4_distortion_correction.py
+++ python_codes/_4_distortion_correction.py
@@ -8,6 +8,7 @@ import numpy as np
 # create marker volumes:
 mr_volume_with_DC = MarkerVolume(r'../_segmentation_data/GOAM_MRI/2022-08__Studies/GOAM^ImageX_ZZZIMAGEX_MR_2022-08-31_172341_._T2.3D.Tra.2min_n301__00002/slicer_centroids.mrk.json')
 mr_volume_no_DC = MarkerVolume(r'../_segmentation_data/GOAM_MRI/2022-08__Studies/GOAM^ImageX_ZZZIMAGEX_MR_2022-08-31_172341_._T2.3D.Tra.2min_n301__00001/slicer_centroids.mrk.json')
+mr_volume_no_DC.MarkerCentroids.y = mr_volume_no_DC.MarkerCentroids.y - mr_volume_no_DC.MarkerCentroids.y.mean()
 ct_volume = MarkerVolume(r'../_segmentation_data/GOAM_CT/slicer_centroids.mrk.json')
 
 # match markers:
@@ -16,6 +17,13 @@ with_DC_match = MatchedMarkerVolumes(ct_volume, mr_volume_with_DC, n_refernce_ma
 
 # calculate harmonics:
 Bz_field = ConvertMatchedMarkersToBz(no_DC_match.MatchedCentroids, mr_volume_no_DC.dicom_data)
+# calculate fields at central marker
+distance_vector = Bz_field.MagneticFields[['x', 'y', 'z']]
+distance_vector = distance_vector.to_numpy()
+distance = np.linalg.norm(distance_vector, axis=1)
+zero_ind = np.argmin(distance)
+print(f'calculated fields at central marker: {Bz_field.MagneticFields.iloc[zero_ind]}')
+
 gradient_strength = np.array(mr_volume_no_DC.dicom_data['gradient_strength'])
 normalisation_factor = [1 / gradient_strength[0], 1 / gradient_strength[1], 1 / gradient_strength[2],
                         1]  # this normalised gradient harmonics to 1mT/m
@@ -24,15 +32,19 @@ G_x_Harmonics, G_y_Harmonics, G_z_Harmonics, B0_Harmonics = calculate_harmonics(
                                                                                 norm=normalisation_factor)
 
 
-dis_data_loc = Path(r'/home/brendan/Downloads/GOAM/GOAM_MRI/2022-08__Studies/GOAM^ImageX_ZZZIMAGEX_MR_2022-08-31_172341_._T2.3D.Tra.2min_n301__00001')
-
-GDC = KspaceDistortionCorrector(ImageDirectory=dis_data_loc.resolve(),
+dis_data_loc = Path(r'C:\Users\Brendan\Downloads\GOAM-01092022_Genesis\GOAM\GOAM_MRI\2022-08__Studies\GOAM^ImageX_ZZZIMAGEX_MR_2022-08-31_172341_._T2.3D.Tra.2min_n301__00001')
+GDC = ImageDomainDistortionCorrector(ImageDirectory=dis_data_loc.resolve(),
                                 gradient_harmonics=[G_x_Harmonics.harmonics,
                                                     G_y_Harmonics.harmonics,
                                                     G_z_Harmonics.harmonics],
                                 dicom_data=mr_volume_no_DC.dicom_data,
-                                correct_through_plane=True)
+                                correct_through_plane=False)
 
 GDC.correct_all_images()
 GDC.save_all_images()  # saves as png so you can quickly inspect results
 GDC.save_all_images_as_dicom()  # saves as dicom which can be read into analysis packages.
+
+
+corrected_volume = MarkerVolume(dis_data_loc / 'corrected_dcm')  # this takes a while
+corrected_volume.export_to_slicer()
+corrected_volume.save_dicom_data()
